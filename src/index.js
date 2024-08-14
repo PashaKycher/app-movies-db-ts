f9bf6ccf97cd69e568b9ebbcde03e755af4ab988
Pavlo <Pasha.Kycher89@gmail.com>
2024-08-15 01:06:40 +0300

    Publication of the first version 0.6.0.27

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
     </Provider>),
     children: [
       {
-        path: "/app-movies-db-ts/",
+        path: "/",
         element: <Home />,
       },
       {
